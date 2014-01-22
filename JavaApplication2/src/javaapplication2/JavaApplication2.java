f5f5db64ad38a8286fa2a0f747fa3caca02210b2
Carlos David Ortiz Muñiz <carlosdavid.om@gmail.com>
2014-01-21 21:28:45 -0600

    cambie vampirazo

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -16,7 +16,7 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Mi primer proyecto que noes spring :((((");
+        System.out.println("Mi primer proyecto que noes spring :(((");
     }
     
 }
